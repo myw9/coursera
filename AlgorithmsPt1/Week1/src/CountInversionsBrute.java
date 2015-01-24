7072b7a44f1e917f5e5bad1dbb3b91b20a0e82f4
Michael Wu <michaelywu9@gmail.com>
2015-01-24 14:47:58 -0500

    Created new java project for Week 1 assignment

---
+++ AlgorithmsPt1/Week1/src/CountInversionsBrute.java
@@ -0,0 +1,9 @@
+
+public class CountInversionsBrute {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Count Inversions Brute Force");
+	}
+
+}
